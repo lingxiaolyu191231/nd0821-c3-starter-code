0feea90a4fc7b35d9ec4f757e43cc415b22cb17b
Lingxiao Lyu <78327908+lingxiaolyu191231@users.noreply.github.com>
2021-09-08 17:13:29 -0700

    add .dvc

--- starter/main.py
+++ starter/main.py
@@ -18,8 +18,11 @@ from sklearn.model_selection import train_test_split
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
-    if os.system("dvc pull") != 0:
-        exit("dvc pull failed")
+    dvc_output = subprocess.run(["dvc", "pull"], capture_output=True, text=True)
+    print(dvc_output.stdout)
+    print(dvc_output.stderr)
+    if dvc_output.returncode != 0:
+        print("dvc pull failed")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 class Input(BaseModel):
