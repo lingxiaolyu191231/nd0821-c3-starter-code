11c6f6f492ab7c105de1696dc8d038e83ef45801
Lingxiao Lyu <78327908+lingxiaolyu191231@users.noreply.github.com>
2021-09-20 10:23:27 -0700

    change png name and fix slice_data function

--- starter/starter/compute_metrics_on_slice_data.py
+++ starter/starter/compute_metrics_on_slice_data.py
@@ -25,36 +25,50 @@ cat_features = [
     "native-country",
 ]
 
-def compute_metrics_on_data_slice(categorical_feature, 
-                                  categorical_feature_value,
+def compute_metrics_on_data_slice(categorical_feature,
                                   data=data,
                                   model = load_gbc,
                                   encoder = encoder,
                                   lb = lb
                                   ):
-    X_slice, y_slice, _, _ = process_data(data, 
-                                    categorical_features=cat_features,
-                                    label = 'salary',
-                                    training=False,
-                                    encoder=encoder,
-                                    lb=lb
-    )
-    preds = inference(model, X_slice)
-    precision, recall, fbeta = compute_model_metrics(y_slice, preds)
-
+    feature_value_list = []
+    precision_value = []
+    recall_value = []
+    fbeta_value = []
+    n = len(data[categorical_feature].unique())
+    
+    for f_value in data[categorical_feature].unique():
+        data_slice = data.loc[data[categorical_feature] == f_value, ]
+        X_slice, y_slice, _, _ = process_data(data_slice,
+                                        categorical_features=cat_features,
+                                        label = 'salary',
+                                        training=False,
+                                        encoder=encoder,
+                                        lb=lb
+        )
+        
+        preds = inference(model, X_slice)
+        precision, recall, fbeta = compute_model_metrics(y_slice, preds)
+        
+        feature_value_list.append(f_value)
+        precision_value.append(precision)
+        recall_value.append(recall)
+        fbeta_value.append(fbeta)
+        
     with open('../screenshots/slice_output.txt', 'w') as f:
         f.write(f'metrics on slice data\ncategorical feature: {categorical_feature}\n')
-        f.write(f'categorical_feature_value: {categorical_feature_value}\n')
-        f.write('\n')
-        f.write('precision: ')
-        f.write(str(precision))
-        f.write('\n')
-        f.write('recall: ')
-        f.write(str(recall))
-        f.write('\n')
-        f.write('fbeta: ')
-        f.write(str(fbeta))
-    
+        for i in range(n):
+            f.write(f'categorical_feature_value: {feature_value_list[n]}\n')
+            f.write('\n')
+            f.write('precision: ')
+            f.write(str(precision_value[n]))
+            f.write('\n')
+            f.write('recall: ')
+            f.write(str(recall_value[n]))
+            f.write('\n')
+            f.write('fbeta: ')
+            f.write(str(fbeta_value[n]))
+        
     f.close()
 
 def main():
@@ -62,8 +76,7 @@ def main():
         categorical_feature = sys.argv[1]
         categorical_feature_value = sys.argv[2]
 
-        compute_metrics_on_data_slice(categorical_feature, 
-                                  categorical_feature_value,
+        compute_metrics_on_data_slice(categorical_feature,
                                   data=data,
                                   model = load_gbc,
                                   encoder = encoder,
